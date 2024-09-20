58b8fb53bc031cccf0f3ac3c1942b1ed6cd93893
vinay kumar <vkumar8192449@gmail.com>
2024-09-21 03:52:53 +0530

    Removed compiled JS files and update .gitignore

--- src/routes/moviesRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// routes/moviesRoutes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const moviesController_1 = require("../controllers/moviesController");
-const router = (0, express_1.Router)();
-router.post("/add-movieshow", moviesController_1.createMoviesController);
-exports.default = router;
