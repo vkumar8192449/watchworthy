b63fcae18bb99775dbfddc5c29211622ea75e729
vinay kumar <vkumar8192449@gmail.com>
2024-09-21 12:23:11 +0530

    User validation add

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,17 +2,32 @@
 
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import validator from "validator";
+import { passwordStrength } from "check-password-strength";
 
 const prisma = new PrismaClient();
 
 export const createUserController = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
+
   try {
+    if (
+      !validator.isEmail(email) ||
+      username.length < 6 ||
+      !validator.isAlpha(username)
+    ) {
+      throw new Error("Invalid inputs");
+    }
+
+    if (passwordStrength(password).id < 2) {
+      throw new Error("Enter strong password");
+    }
+
     const user = await prisma.user.create({
       data: { username, email, password },
     });
     res.json(user);
-  } catch (error) {
-    res.status(400).json({ error: "User creation failed" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message || "User creation failed" });
   }
 };
