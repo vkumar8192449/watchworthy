64fa0cb5c10f9f831ffb1d1ac9e198aa46b21ebb
vinay kumar <vkumar8192449@gmail.com>
2024-09-21 12:32:52 +0530

    Movie/show validation add

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -2,17 +2,22 @@
 
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import validator from "validator";
 
 const prisma = new PrismaClient();
 
 export const createMoviesController = async (req: Request, res: Response) => {
   const { title, genre, release_year, description } = req.body;
   try {
+    if (!validator.isNumeric(release_year.toString())) {
+      throw new Error("Invalid release year");
+    }
+
     const movie = await prisma.movie.create({
       data: { title, genre, release_year, description },
     });
     res.json(movie);
-  } catch (error) {
-    res.status(400).json({ error: "Movie creation failed" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message || "Movie creation failed" });
   }
 };
